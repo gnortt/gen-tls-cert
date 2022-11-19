28ae121ef8b8457d942c53a775ef0e8792f1388a
gnortt <115700190+gnortt@users.noreply.github.com>
2022-11-19 21:33:23 +0100

    updated openssl req parameters

--- gen-tls-cert.sh
+++ gen-tls-cert.sh
@@ -15,32 +15,31 @@ DAYS=$5
 
 mkdir "$OUT_DIR"
 OUT_DIR="$(pwd)/$OUT_DIR"
-
-SERIAL=01
-echo $SERIAL > "$OUT_DIR"/serial
 touch "$OUT_DIR"/index.txt
 
 export KEY_DIR="$OUT_DIR"
 export KEY_CN="$CA_CN"
+
 openssl req \
     -config "openssl.cnf" \
     -batch \
-    -nodes \
+    -noenc \
     -x509 \
-    -new \
-    -sha256 \
+    -sha384 \
     -days $DAYS \
-    -newkey rsa:$KEY_SIZE \
+    -new \
     -newkey ec:<(openssl ecparam -name secp384r1) \
     -keyout "$OUT_DIR"/ca.key \
     -out "$OUT_DIR"/ca.crt
 
 export KEY_CN="$SERVER_CN"
+
 openssl req \
     -config "openssl.cnf" \
     -batch \
-    -nodes \
+    -noenc \
     -extensions server \
+    -sha384 \
     -new \
     -newkey ec:<(openssl ecparam -name secp384r1) \
     -keyout "$OUT_DIR/$SERVER_CN.key" \
@@ -50,8 +49,8 @@ openssl ca \
     -config "openssl.cnf" \
     -batch \
     -notext \
+    -rand_serial \
     -extensions server \
-    -md sha256 \
     -days $DAYS \
     -in "$OUT_DIR/$SERVER_CN.csr" \
     -out "$OUT_DIR/$SERVER_CN.crt"
